a685f13c4473e6aa43f7db5d281ef9cf1ff0c7a4
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-10 17:15:24 +0100

    reference assets

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pixel Positions</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
 
     <div>
         <nav>
             <div>
-                logo
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
             </div>
 
             <div>
