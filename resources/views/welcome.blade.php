d5ada9aae293bf17520e1555aeaf96337f34e415
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-10 23:32:11 +0100

    create placeholders for another sections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,19 @@
 <x-layout>
-    <section>
-        <x-section-heading>Featured Jobs</x-section-heading>
+    <div class="space-y-10">
+        <section>
+            <x-section-heading>Featured Jobs</x-section-heading>
 
-        <div class="grid lg:grid-cols-3 gap-8 mt-6">
-            <x-job-card/>
-            <x-job-card/>
-            <x-job-card/>
-        </div>
-
-    </section>
+            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                <x-job-card/>
+                <x-job-card/>
+                <x-job-card/>
+            </div>
+        </section>
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+        </section>
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
 </x-layout>
