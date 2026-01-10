89c95c48d70b7127a4167962fb4b46fe2d670deb
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-10 22:54:27 +0100

    Create first element of the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 <x-layout>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <section>
+        <h3>
+            Top Jobs
+        </h3>
+
+        <div>
+            <div>
+                <div>Employer Name</div>
+                <div>
+                    <div>Title</div>
+                    <div>Salary</div>
+                </div>
+                <div>
+                    <div>
+                        <a href="#">Tag</a>
+                        <a href="#">Tag</a>
+                        <a href="#">Tag</a>
+                    </div>
+
+                    <img src="https://placehold.it/42/42" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
 </x-layout>
