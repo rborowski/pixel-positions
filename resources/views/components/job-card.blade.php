1f804de89e9e45bbc9ed2c52b0d1810ab2c7e010
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-10 23:22:03 +0100

    create job-card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Employer Name</div>
+    <div class="py-8 font-bold">
+        <div>Title</div>
+        <div>Salary</div>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
+        </div>
+
+        <img class="rounded-xl " src="http://placeholdit.com/42/42" alt="">
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,10 @@
         <h3 class="mb-6 ">
             Top Jobs
         </h3>
-
-        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-            <div class="self-start text-sm">Employer Name</div>
-            <div class="py-8 font-bold">
-                <div>Title</div>
-                <div>Salary</div>
-            </div>
-            <div class="flex justify-between items-center mt-auto">
-                <div>
-                    <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
-                    <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
-                    <a href="#" class="bg-white/10 hover:bg-white/25 rounded-2xl px-2 py-1 text-xs transition-colors duration-300">Tag</a>
-                </div>
-
-                <img class="rounded-xl " src="http://placeholdit.com/42/42" alt="">
-            </div>
+        <div class="grid lg:grid-cols-3 gap-8 ">
+            <x-job-card/>
+            <x-job-card/>
+            <x-job-card/>
         </div>
 
     </section>
