b299f35a19710c939aa7d6885985c2a5bb01e4ec
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-10 16:48:02 +0100

    add skeleton for layout page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <title>Pixel Positions</title>
 </head>
 <body>
 
+    <div>
+        <nav>
+            <div>
+                logo
+            </div>
+
+            <div>
+                links
+            </div>
+            <div>
+                post a job
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 </body>
 </html>
