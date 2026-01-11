505aa8c55b1a1eb9c7bef9668f29192d730a5e8d
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 13:49:29 +0100

    change weight bold

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,7 +1,7 @@
 <a
     {{ $attributes }}
     href="#"
-    class="bg-white/10 mr-1 hover:bg-white/25 rounded-xl px-2 py-1 text-xs transition-colors duration-300"
+    class="bg-white/10 mr-1 hover:bg-white/25 rounded-xl px-3 py-1 text-2xs font-bold transition-colors duration-300"
 >
     {{ $slot }}
 </a>
