058b88e1995ebfce5ca9b076c8dee7b13c718471
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 16:15:42 +0100

    Add jobs relationship to employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
@@ -14,5 +15,10 @@ class Employer extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
     }
 }
