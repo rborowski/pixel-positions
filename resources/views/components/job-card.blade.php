f3eba39d10751e0610cc0efe6e9140dc6f6b37fb
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 14:07:36 +0100

    change font weight

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,8 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group">
     <div class="self-start text-sm">Employer Name</div>
-    <div class="py-8 font-bold">
-        <h3>Title</h3>
-        <p>Salary</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 text-xl font-bold">Title</h3>
+        <p class="text-sm mt-4">Salary</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
