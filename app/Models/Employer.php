a660f21bad080a2763995c440f234c2df4856aa3
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 16:12:12 +0100

    Add user relationship to employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
