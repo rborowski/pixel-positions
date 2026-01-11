1908d897e9131115a06008999760f90d67eb8247
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 16:18:09 +0100

    Set up an Employer factory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class EmployerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'logo' => $this->faker->imageUrl(),
+            'user_id' => User::factory(),
         ];
     }
 }
