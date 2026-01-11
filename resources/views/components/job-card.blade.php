78031681f05ca83e2705dfbc783f11c431e85873
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-11 14:12:21 +0100

    add transition

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,7 +1,7 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group">
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-600 group transition-colors duration-300">
     <div class="self-start text-sm">Employer Name</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold">Title</h3>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Title</h3>
         <p class="text-sm mt-4">Salary</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
