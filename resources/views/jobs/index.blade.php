08df7135cfb58e01d430008a79220c0e4d74e52a
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 20:37:45 +0100

    Use form components for searchbar

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,9 +3,10 @@
         <section class="text-center pt-6">
             <h1 class="text-center font-bold text-4xl ">Let's Find Your Next Job</h1>
 
-            <form action="">
-                <input type="text" placeholder="Web Developer..." class="mt-6 px-5 py-4 w-full max-w-xl border rounded-xl bg-white/5 border-white/10"/>
-            </form>
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input :label="false" name="q" placeholder="Web Developer..."/>
+            </x-forms.form>
+
         </section>
 
         <section class="pt-10">
