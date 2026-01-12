064150c48c42664eef983c57e7c55b621f54b86a
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 04:00:00 +0100

    Prepare seeders

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class JobSeeder extends Seeder
@@ -12,6 +13,7 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = Tag::factory(3)->create();
+        Job::factory(20)->hasAttached($tags)->create();
     }
 }
