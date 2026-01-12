fcd5e996e5b86b55f239a2f7b0054b58e7050d56
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 06:30:00 +0100

    Create login view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-page-heading>Register</x-page-heading>
+
+    <x-forms.form method="POST" action="/login" >
+
+        <x-forms.input label="Email" name="email" type="email" />
+        <x-forms.input label="Password" name="password" type="password" />
+
+        <x-forms.button>Log in</x-forms.button>
+    </x-forms.form>
+</x-layout>
