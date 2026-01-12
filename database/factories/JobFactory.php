d15b92db285d6dbe1f4f778d7ad2a5477221c8e1
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 01:00:00 +0100

    Set up a Job factory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,14 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => $this->faker->jobTitle(),
+            'salary' => $this->faker->randomElement(['50,000 USD', '60,000 USD', '100,000 USD', '150,000 USD', '90,000 USD']),
+            'location' => $this->faker->randomElement(['Remote', 'Hybrid', 'On-site']),
+            'schedule' => $this->faker->randomElement(['Full Time', 'Part Time', 'Freelance']),
+            'url' => $this->faker->url(),
+            'featured' => false
+
         ];
     }
 }
