0375bf00f396e6177104dea58010dffdd4e35f25
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 06:15:00 +0100

    Change name of employer faker in factory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -18,7 +18,7 @@ class EmployerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->company(),
             'logo' => $this->faker->imageUrl(),
             'user_id' => User::factory(),
         ];
