a966a32bc5afc220925e90b3db5c70dcd10bc2c8
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-12 21:04:34 +0100

    Create __invoke method

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
 
+    public function __invoke(Request $request)
+    {
+        $jobs = Job::where('title', 'LIKE', '%' . request('q') . '%')->get();
+
+        return $jobs;
+    }
 }
