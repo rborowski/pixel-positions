ec87932bb9cefdd91efcb2379f39105c48e7c123
Rafał Borowski <rborowski.gdy@gmail.com>
2026-01-15 21:35:18 +0100

    Add "No results found" to results

--- resources/views/components/job/results.blade.php
+++ resources/views/components/job/results.blade.php
@@ -2,8 +2,15 @@
     'jobs'
 ])
 
-<div class="space-y-6">
-    @foreach($jobs as $job)
-        <x-job.card-wide :$job />
-    @endforeach
-</div>
+@if($jobs->isEmpty())
+    <div class="text-center py-12">
+        <p class="text-gray-400 text-lg mb-2">Oops, no results found</p>
+        <p class="text-gray-500">Try changing your search criteria</p>
+    </div>
+@else
+    <div class="space-y-6">
+        @foreach($jobs as $job)
+            <x-job.card-wide :$job />
+        @endforeach
+    </div>
+@endif
